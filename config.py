effd4e49bb3feb81112ce316d502ee3a7387222d
yellowsoar <yellowsoar@gmail.com>
2024-04-22 19:16:08 +0800

    add: configs

---
+++ config.py
@@ -0,0 +1,13 @@
+MONGODB_SETTINGS = {
+    'db': 'ccip',
+    'host': '127.0.0.1',
+    'port': 27017
+}
+
+SCENARIO_DEFS = {
+    "attendee": "scenario-attendee.json",
+    "staff": "scenario-staff.json",
+    "speaker": "scenario-speaker.json"
+}
+
+ANNOUNCEMENT_DEFAULT_ROLE = "attendee"
